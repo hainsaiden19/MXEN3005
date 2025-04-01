5fbe7a112ef0954080d8c924bef32f15e284e12b
hainsaiden19 <hainsaiden19@gmail.com>
2025-04-01 19:08:22 +0800

    wx250s visualisation basics complete

--- milestone5_ws/src/wx250s_viz/setup.py
+++ milestone5_ws/src/wx250s_viz/setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+import os
+from glob import glob
+
 package_name = 'wx250s_viz'
 
 setup(
@@ -10,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
