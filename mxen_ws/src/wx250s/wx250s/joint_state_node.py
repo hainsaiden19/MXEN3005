20ed074455094bd4356b2d5dfb384df49a6099d4
Aiden <hainsaiden19@gmail.com>
2025-04-11 10:13:30 +0800

    milestone 5 complete

--- mxen_ws/src/wx250s/wx250s/joint_state_node.py
+++ mxen_ws/src/wx250s/wx250s/joint_state_node.py
@@ -1,6 +1,9 @@
 import rclpy
 from rclpy.node import Node
 
+from math import radians
+import numpy as np
+
 from sensor_msgs.msg import JointState
 
 from xarmclient import XArm
@@ -16,12 +19,16 @@ class JointStateNode(Node):
 
         ### Publishing Creation ##
         self.publisher = self.create_publisher(JointState, "/joint_state", 10)
-        timer_period = 0.2
+        timer_period = 0.05
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     ### Publishing Execution ###
     def timer_callback(self):
         self.msg.position = self.xarm.get_joints()
+        self.msg.name = ["joint1", "joint2", "joint3", "joint4", "joint5", "joint6"]
+
+        self.msg.position = [radians(j) for j in self.msg.position]
+
         self.publisher.publish(self.msg)
 
 def main(args=None):
